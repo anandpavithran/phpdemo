9c62bf0e8dc5ccc27c37c468dd4e42cd46974c9f
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2021-07-28 10:25:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 <?php
-ECHO "Hello World!GOODAFTERNOON<br>";
-echo "Hello World!AFTER LUNCH BREAK<br>";
-EcHo "Hello World!<br>";
+echo "Hello World!GOODMORNING<br>";
+echo "Hello World!<br>";
+echo "Hello World!<br>";
 ?>
 
 
