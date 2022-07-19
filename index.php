7151c74f1f831cfb6341c832bff9afa90a5fc6fe
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2022-07-19 12:50:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 </head>
 <body>  
-  <b>ERICSSON</b>
+  <b>ERICSSON UPDATED JULY19</b>
 <?php
 // define variables and set to empty values
 $name = $email = $gender = $comment = $website = "";
