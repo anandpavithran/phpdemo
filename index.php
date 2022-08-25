740d0ece741ed972468347d4f5c74214d0dad4eb
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2022-08-25 12:04:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 </head>
 <body>  
-  <b>NIC UPDATED AUGUST 18</b>
+  <b>VODAFONEIDEA UPDATED AUGUST 18</b>
 <?php
 // define variables and set to empty values
 $name = $email = $gender = $comment = $website = "";
